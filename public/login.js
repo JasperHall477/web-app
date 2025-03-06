155f0ac83ef0c5a2b45c5c79b712e14b67940dd1
Jaspers\Jasper <Jasperhall477@gmail.com>
2025-03-06 23:50:08 +0000

    fixed login with updated cors - for render deploym

--- public/login.js
+++ public/login.js
@@ -5,7 +5,8 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
   const password = document.getElementById('password').value;
 
   try {
-    const response = await fetch('/login', {
+    //const response = await fetch('/login', {
+    const response = await fetch('https://web-app-j994.onrender.com/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password }),
